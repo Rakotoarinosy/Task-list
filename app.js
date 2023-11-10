7e56e2f01e28392ede33f0a8ebebf394a7e44e20
Rakotoarinosy <fehizoroarinosy@gmail.com>
2023-11-10 06:10:05 +0300

    modification of addTask function

--- app.js
+++ app.js
@@ -58,29 +58,35 @@ function addTask(e){
     alert('Add a task');
   }
 
-  // Create li element
-  const li = document.createElement('li');
-  // Add class
-  li.className = 'collection-item';
-  // Create text node and append
-  li.appendChild(document.createTextNode(taskInput.value));
-  // Create new link element
-  const link = document.createElement('a');
-  // Add class
-  link.className = 'delete-item secondary-content';
-  //Add icon html
-  link.innerHTML = '<i class="fa fa-remove"></i>';
-  //Append the link to li
-  li.appendChild(link);
+  else{
+    
+    // Create li element
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item';
+    // Create text node and append
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create new link element
+    const link = document.createElement('a');
+    
+    // Add class
+    link.className = 'delete-item secondary-content';
 
-  //Append li to ul
-  taskList.appendChild(li);
+    //Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
 
-  //Store in LS
-  storeTaskInLocalStorage(taskInput.value);
+    //Append the link to li
+    li.appendChild(link);
 
-  //Clear input
-  taskInput.value = '';
+    //Append li to ul
+    taskList.appendChild(li);
+
+    //Store in LS
+    storeTaskInLocalStorage(taskInput.value);
+    
+    //Clear input
+    taskInput.value = '';
+}
 
   e.preventDefault();
 }
